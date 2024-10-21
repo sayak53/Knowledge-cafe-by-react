0923e585ec4a64fde886b4b37d9a4d0f01ff7712
sayak53 <sayakchakraborty928@gmail.com>
2024-10-21 20:00:48 +0530

    header section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 import Blogs from './components/blogs/Blogs'
+import profile from './assets/images/profile.png'
 function App() {
 
   return (
     <>
-      <h1 className='font-bold text-4xl p-5'> Knowlege Cafe</h1>
+      <div className='flex w-11/12 mx-auto justify-between items-center p-3'>
+        <h1 className='font-bold text-4xl'> Knowlege Cafe</h1>
+        <img src={profile} alt="" />
+      </div>
       <Blogs></Blogs>
     </>
   )
