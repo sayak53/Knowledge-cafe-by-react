94155d5e28398c7c222a26c158a00a330c7f7788
sayak53 <sayakchakraborty928@gmail.com>
2024-10-21 19:37:50 +0530

    fake data created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
-
+import Blogs from './components/blogs/Blogs'
 function App() {
 
   return (
     <>
-      <h1 className='font-bold text-4xl p-5'>Knowlege Cafe</h1>
+      <h1 className='font-bold text-4xl p-5'> Knowlege Cafe</h1>
+      <Blogs></Blogs>
     </>
   )
 }
