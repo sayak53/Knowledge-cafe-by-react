ea71c6d6262c422e6f2b12bff2e1300d8f291cef
sayak53 <sayakchakraborty928@gmail.com>
2024-10-22 12:56:46 +0530

    almost 60% work completed

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,12 +1,27 @@
 
 const Blog = ({blog}) => {
     
-    const{cover_title,author_img} = blog
+    const{cover_title,author_img,cover_image,reading_time,author,posted_date,hashtags} = blog
 
     return (
         <div>
-            <img className="w-[400px]" src={author_img} alt="" />
+          <img className="w-[400px]" src={author_img} alt="" />
+          <div className="flex justify-between items-center">
+            <div className="flex">
+               <img className="w-[50px] h-[50px] rounded-full" src={cover_image} alt="" />
+               <div className="ml-6">
+                 <h3 className="text-2xl font-bold">{author}</h3>
+                 <p className="text-base text-gray-600">Posted on {posted_date}</p>
+               </div>
+            </div>
+            <div>
+               <span>{reading_time} mins read</span>
+            </div>
+          </div>
           <h2 className="text-4xl">{cover_title}</h2>
+          {
+             hashtags.map((hash,idx) => <span key={idx}><a className="text-violet-900" href="#"> {hash} </a></span>)
+          }
         </div>
     );
 };
